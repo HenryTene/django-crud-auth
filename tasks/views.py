0f4a620a3eb8494e5cd05f158201d24fdfde052e
HenryTene <henry_tene@hotmail.com>
2025-01-08 10:37:24 -0500

    :sparkles: Se agrega funcion para completar y eliminar tareas

--- tasks/views.py
+++ tasks/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
 from .forms import TaskForm
 from .models import Task
+from django.utils import timezone
 
 
 # Create your views here.
@@ -103,3 +104,16 @@ def task_detail(request, task_id):
                 'form': form,
                 'error': 'Bad data passed in. Try again'
             })
+
+def complete_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id, user = request.user)
+    if request.method == 'POST':
+        task.datecompleted = timezone.now()
+        task.save()
+        return redirect('tasks')
+    
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id, user = request.user)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('tasks')
